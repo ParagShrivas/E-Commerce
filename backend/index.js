134721b8c7352d862010f918fe284d7a1563e7aa
noureennaaz <noureennaaz09@gmail.com>
2024-08-25 09:36:54 +0530

    Added the cart functions

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const Auth = require('./modules/Auth');
 const products = require('./modules/products');
 const users = require('./modules/users');
+const cart =require('./modules/cart')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/login', Auth);
 app.use('/products', products);
 app.use('/users', users);
-
+app.use('/cart', cart);
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
