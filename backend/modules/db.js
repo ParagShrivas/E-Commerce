5948798fd1703393ba31f44deb922bdb65fb4400
ParagShrivas <121390469+ParagShrivas@users.noreply.github.com>
2025-04-13 20:08:03 +0530

    Update db.js

--- backend/modules/db.js
+++ backend/modules/db.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
+
 const { Pool } = require("pg");
 
-// const isProduction = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const query = new Pool({
-     connectionString: `postgresql://parag:f8d2Tp4RQNu6lHu2ylJKNpluJn40PabW@dpg-cvtq7495pdvs73dv1ao0-a.oregon-postgres.render.com:5432/ecommerce_zg51`,
-     ssl:  {
-               rejectUnauthorized: false,
-          }
-          });
+     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+     ssl: isProduction
+});
 
 module.exports = query;
